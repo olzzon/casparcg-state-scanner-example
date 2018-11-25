9317e18615af2c19351e844b3f1480b354a9753b
olzzon <github@olzzon.dk>
2018-11-25 15:46:04 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,44 +1,18 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## CasparCG-State-Scanner Client Example
 
-## Available Scripts
+A simple Client that subscribe realtime from CasparCG-State-Scanner via: 
+```
+const CCG_SERVER_IP = "192.7.9.13"
+```
+Connects to CasparCG-State-Scanner via port: 5254 
 
-In the project directory, you can run:
+Usind Apollo and GraphQL subscriptions gives you realtime updates from your server.
+Try to run it on a server antoher client is using. And see the instant response :)
 
-### `npm start`
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## It´s Build on Create-React-App
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+To keep the example as simple and easy to redo if you wan´t to try out yourself it´s a basic "create-react-app" and all changes has been put into App.js
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Just do a Yarn and a Yarn start to run the app.
+(Install Yarn and Nodejs if you don´t have it on your machine)
--- src/App.js
+++ src/App.js
@@ -2,23 +2,175 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Apollo-Client Graphql implementation:
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from "graphql-tag";
+
+//Constants:
+const CCG_SERVER_IP = "localhost";
+
+//Create GraphlQL queries 
+//(usually placed in separate .js file)
+
+const channelQuery = {
+  query: gql`
+    query {
+      channels {
+        layers {
+          foreground {
+            name
+            path
+            length
+            loop
+            paused
+          }
+          background {
+            name
+            path
+            length
+            loop
+          }
+        }
+      }
+    }`
+};
+
+const channelSubscribe = {
+  query: gql`
+    subscription {
+      channels {
+        layers {
+          foreground {
+            name
+            path
+            length
+            loop
+            paused
+          }
+          background {
+            name
+            path
+            length
+            loop
+          }
+        }
+      }
+    }`
+};
+
+const timeLeftSubscribe = {
+  query: gql`
+    subscription {
+      timeLeft {
+        timeLeft
+      }
+    }`
+};
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ccgChannelsRendered: [],
+      ccgTimeLeftRendered: []
+    };
+    this.renderChannels = this.renderChannels.bind(this);
+    this.renderTimeLeft = this.renderTimeLeft.bind(this);
+  }
+
+  componentDidMount() {
+    // Initialize GraphQL with Query and Subscription.
+    // You can use WebsocketLink for both Query and Subscriptions
+
+    const wsLink = new WebSocketLink({
+      uri: "ws://" + CCG_SERVER_IP + ":5254/graphql",
+      options: {
+        reconnect: true
+      }
+    });
+
+    this.ccgStateConnection = new ApolloClient({
+        link: wsLink,
+        cache: new InMemoryCache(),
+        defaultOptions: {
+            watchQuery: {
+                fetchPolicy: 'cache-and-network',
+                errorPolicy: 'ignore',
+            },
+            query: {
+                fetchPolicy: 'network-only',
+                errorPolicy: 'all',
+            },
+            mutate: {
+                errorPolicy: 'all'
+            }
+        }
+    });
+
+    //Initial Query state of CasparCG.
+    //As subscription does not give a default state.
+    this.ccgStateConnection.query(channelQuery)
+    .then((response) => {
+      this.setState({ ccgChannelsRendered: this.renderChannels(response.data.channels)});
+    });
+
+    //Subscribe to CasparCG-State changes:
+    const _this2 = this; //reference for this. As subscribe creates new this
+
+    this.ccgStateConnection.subscribe(channelSubscribe)
+    .subscribe({
+      next(response) {
+        _this2.setState({ ccgChannelsRendered: _this2.renderChannels(response.data.channels)});
+      },
+      error(err) { console.error('Subscription error: ', err); },
+    });
+
+    //Subscribe timer countdown on CastarCG-State:
+    this.ccgStateConnection.subscribe(timeLeftSubscribe)
+    .subscribe({
+      next(response) {
+        _this2.setState({ccgTimeLeftRendered: _this2.renderTimeLeft(response.data.timeLeft)});
+      },
+      error(err) { console.error('Subscription error: ', err); },
+    });
+
+  }
+
+  renderChannels(channels) {
+      return(
+        <div>
+          Foreground:
+          <br/>
+          Name: {channels[0].layers[9].foreground.name}
+          <br/>
+          Length: {channels[0].layers[9].foreground.length}
+        </div>
+      )
+  }
+
+  renderTimeLeft(timeLeft) {
+    return(
+      <div>
+        TimeLeft: {timeLeft[0].timeLeft}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Channel 1:
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            {this.state.ccgChannelsRendered}
+            <br/>
+            {this.state.ccgTimeLeftRendered}
+          </div>
         </header>
       </div>
     );
